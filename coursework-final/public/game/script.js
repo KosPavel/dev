268bd44ca6c7c52453afa2d72c46193251cdc7f6
Pavel Kos <kos.pav007@yandex.ru>
2019-02-09 09:52:53 +0300

    beta

---
+++ coursework-final/public/game/script.js
@@ -0,0 +1,46 @@
+document.body.style.backgroundColor = 'rgba(255, 242, 106, 1)';
+
+
+// document.body.style.backgroundImage = "url('standoff.jpg')";
+// document.body.style.backgroundRepeat = "no-repeat";
+// document.body.style.backgroundSize = "cover";
+
+// let input = document.querySelector(".input");
+// let button = document.querySelector(".button");
+// let ul = document.querySelector(".ul");
+// let socket = io.connect(location.href);
+
+// let name = prompt('Введите имя');
+
+// button.onclick = function() {
+// 	if (input.value !== '') {
+// 		let li = document.createElement('li');
+// 		li.innerHTML = `${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()} ${name}: ${input.value}`;
+// 		ul.appendChild(li);
+
+// 		socket.emit('from_client', {
+// 			name: name,
+// 			message: input.value,
+// 		});
+
+// 		input.value = '';
+// 	};
+// };
+
+// socket.on('from_server', (data)=>{
+// 	let li = document.createElement('li');
+// 	li.innerHTML = `${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()} ${data.name}: ${data.message}`;
+// 	ul.appendChild(li);
+// });
+
+// socket.on('new_user', (data)=>{
+// 	let li = document.createElement('li');
+// 	li.innerHTML = data;
+// 	ul.appendChild(li);
+// });
+
+// socket.on('user_left', (data)=>{
+// 	let li = document.createElement('li');
+// 	li.innerHTML = data;
+// 	ul.appendChild(li);
+// });
